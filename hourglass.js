74b5904a2477fe7a69a394930dfcc20f55f1d739
David <daviddamilola20@gmail.com>
2021-12-04 22:45:10 +0100

    solved hourglass problem

--- hourglass.js
+++ hourglass.js
@@ -1,21 +1,43 @@
- 0, 2, 4
- 1, 3, 5
+function hourglassSum(arr) {
+  // Write your code here
 
-
- //depth
- {
-     [0, 2, 4] : [0, 2], [3, 5],
-     [1, 3, 5] : [1, 3, 5]
- }
-
- //go depth and move 1 step
-
+  const sumOfPosition = (row, column, arr) => {
+    const top = arr[row - 1][column];
+    const topRight = arr[row - 1][column + 1];
+    const topLeft = arr[row - 1][column - 1];
+    const bottom = arr[row + 1][column];
+    const bottomRight = arr[row + 1][column + 1];
+    const bottomLeft = arr[row + 1][column - 1];
  
+    return (
+      top +
+      topRight +
+      topLeft +
+      bottom +
+      bottomRight +
+      bottomLeft +
+      arr[row][column]
+    );
+  };
 
- //breadth
- // start at index 1
- {
+  const resultArray = [];
 
- }
+  for (let row = 1; row < arr.length - 1; row++) {
+    for (let column = 1; column < arr.length - 1; column++) {
+      resultArray.push(sumOfPosition(row, column, arr));
+    }
+  }
 
+  return Math.max(...resultArray);
+}
 
+console.log(
+  hourglassSum([
+    [-9, -9, -9, 1, 1, 1],
+    [0, -9, 0, 4, 3, 2],
+    [-9, -9, -9, 1, 2, 3],
+    [0, 0, 8, 6, 6, 0],
+    [0, 0, 0, -2, 0, 0],
+    [0, 0, 1, 2, 4, 0],
+  ])
+);
